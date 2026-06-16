39f368c1ed499bfdbc29c3461bbdbce0c87a290f
dimasdaffa <dimas.de2004@gmail.com>
2026-06-16 22:30:01 +0700

    Initial Commit

---
+++ FocalGrid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FocalGrid
+//
+//  Created by DIMAS DAFFA ERNANDA on 16/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FocalGrid/FocalGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocalGridApp.swift
+//  FocalGrid
+//
+//  Created by DIMAS DAFFA ERNANDA on 16/06/26.
+//
+
+import SwiftUI
+
+@main
+struct FocalGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
