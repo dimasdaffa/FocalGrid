6554b022f6488c2a23f725e39503983a40577cb8
dimasdaffa <dimas.de2004@gmail.com>
2026-06-26 14:33:44 +0700

    refactor: update colortheme

--- FocalGrid/Extensions/ColorTheme.swift
+++ FocalGrid/Extensions/ColorTheme.swift
@@ -14,6 +14,9 @@ extension Color {
     static let themeCoast = Color(red: 10/255, green: 78/255, blue: 92/255) // #0A4E5C Pantone Gulf Coast
     static let themeBlue = Color(red: 0/255, green: 75/255, blue: 134/255) // #004B86 Pantone 301 C
     static let themeClay = Color(red: 164/255, green: 73/255, blue: 61/255) // #A4493D Pantone 7608 C
+    static let themeMonument = Color(red: 125/255, green: 134/255, blue: 138/255) // #7D868A Pantone Monument
+    static let themeEclipse = Color(red: 52/255, green: 48/255, blue: 73/255) // #343049 Pantone Eclipse
     static let themeShadow = Color(red: 28/255, green: 31/255, blue: 42/255) // #1C1F2A Pantone 532 C
     static let themeHardShadow = Color(red: 36/255, green: 34/255, blue: 29/255) // #24221D
+    static let themeBlack = Color(red: 17/255, green: 17/255, blue: 17/255) // #111111
 }
